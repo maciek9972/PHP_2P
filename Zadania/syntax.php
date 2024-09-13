d7179c2d12fb4db56e615d2a70f26a1c58002a2e
maciek9972 <mdurlej.com@gmail.com>
2024-09-13 15:38:51 +0200

    Zrobione

---
+++ Zadania/syntax.php
@@ -0,0 +1,25 @@
+<?php
+$a="Tu ";
+$b="funkcja ";
+$c="echo";
+
+echo $a. $b. $c. "<br>";
+echo "Tu funkcja echo"."<br>";
+echo ('Tu funkcja echo')."<br>";
+echo ("Tu funkcja echo")."<br>";
+
+
+
+print "Zgłasza się funkcja print"."<br>";
+print ("Zgłasza się funkcja print")."<br>";
+?>
+<?='hej<br>' ?>
+<?php
+// To jest komentarz jednoliniowy
+# To jest inny komentarz jednoliniowy
+/*
+To jest komentarz
+wieloliniowy
+*/
+echo "Przykład z komentarzami.";
+?>

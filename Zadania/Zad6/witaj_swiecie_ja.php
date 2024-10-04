570c993f780df01f86ada13aa65d8610ee168b32
maciek9972 <mdurlej.com@gmail.com>
2024-10-04 15:38:01 +0200

    Do domu

---
+++ Zadania/Zad6/witaj_swiecie_ja.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Witaj_swiecie_ja</title>
+<style>
+    
+</style>
+</head>
+<body>
+<?php
+if ($_SERVER["REQUEST_METHOD"]=="POST"){
+    $text = "Kon";
+    $Idk =
+}
+?>
+</body>
+</html>

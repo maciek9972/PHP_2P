55a028bb06d7aa129736c5a5cd154db037d51cd7
maciek9972 <85764462+maciek9972@users.noreply.github.com>
2024-11-22 13:48:04 +0100

    Update index.php

--- Zadania2/Masagestosc/index.php
+++ Zadania2/Masagestosc/index.php
@@ -2,7 +2,7 @@
 include_once 'MasaGestosc.php';
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -18,7 +18,7 @@ include_once 'MasaGestosc.php';
     <?php foreach ($substancje as $sub): ?>
         <?php $DodawanieObj = new obliczenia($r, $sub); ?>
         <li>
-            Masa kulki <strong><?= $material ?></strong> o promieniu <u><?= $r ?>cm</u>: <mark><?= $DodawanieObj->masa() ?> kg</mark>
+            Masa kulki <strong><?= $sub ?></strong> o promieniu <u><?= $r ?> cm</u>: <mark><?= $DodawanieObj->masa() ?> kg</mark>
         </li>
     <?php endforeach; ?>
 </ol>

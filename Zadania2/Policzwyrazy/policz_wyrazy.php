24b2460c9e126fbfb7f89a39aeb305c252f7aaca
maciek9972 <85764462+maciek9972@users.noreply.github.com>
2024-12-05 12:09:29 +0100

    Update policz_wyrazy.php

--- Zadania2/Policzwyrazy/policz_wyrazy.php
+++ Zadania2/Policzwyrazy/policz_wyrazy.php
@@ -1,9 +1,11 @@
 <?php
-class PoliczWyrazy{
-    public static string $tekst = "Metoda policzWyrazy() zwraca liczbę wyrazów w danym tekście.";
-    public static function policzWyrazy(string $a){
-        $slowa= explode("",$a);
-       return count($slowa);
-  }
+class PoliczWyraz
+{
+    public static string $tekst = "Metoda policzWyrazy() zwraca liczbę wyrazów w danym tekście";
+    public static function policzWyrazy(string $a): int
+    {
+        $slowa = explode(" ", $a);
+        return count($slowa);
+    }
 }
 ?>

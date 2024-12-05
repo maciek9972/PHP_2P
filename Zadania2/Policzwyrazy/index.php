952d0064645ca1bd0a65416cbff36c6b278120bd
maciek9972 <85764462+maciek9972@users.noreply.github.com>
2024-12-05 12:11:02 +0100

    Update index.php

--- Zadania2/Policzwyrazy/index.php
+++ Zadania2/Policzwyrazy/index.php
@@ -1,28 +1,32 @@
 <?php
-include_once "policz_wyrazy.php";
+include_once("policz_wyrazy.php");
 ?>
 <!DOCTYPE html>
-<html lang="pl">
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-</head>
-<body>
 
-<p><?= PoliczWyrazy::$tekst; ?></p>
-<fildset>
-    <legend>Przykładowy tekst</legend>
-    <pre>
-    Litwo, Ojczyzno moja! ty jesteś jak zdrowie;
-    Ile cię trzeba cenić, ten tylko się dowie,
-    Kto cię stracił. Dziś piękność twą w całej ozdobie
-    Widzę i opisuję, bo tęsknię po tobie.
-    </pre>
-</flidset>
-<p><?= PoliczWyrazy::policzWyrazy("Litwo, Ojczyzno moja! ty jesteś jak zdrowie;
-    Ile cię trzeba cenić, ten tylko się dowie,
-    Kto cię stracił. Dziś piękność twą w całej ozdobie
-    Widzę i opisuję, bo tęsknię po tobie.")?>
+</head>
+
+<body>
+    <p><?= PoliczWyraz::$tekst ?></p>
+
+    <fieldset>
+        <legend>Przykładowy tekst</legend>
+        <pre>
+Litwo! Ojczyzno moja! ty jesteś jak zdrowie.
+Ile cię trzeba cenić, ten tylko się dowie,
+Kto cię stracił. Dziś piękność twą w całej ozdobie
+Widzę i opisuję, bo tęsknię po tobie.
+        </pre>
+    </fieldset>
+    <p>Tekst w ramce składa się z
+        <?= PoliczWyraz::policzwyrazy("Litwo! Ojczyzno moja! ty jesteś jak zdrowie. Ile cię trzeba cenić, ten tylko się dowie, Kto cię stracił. Dziś piękność twą w całej ozdobie Widzę i opisuję, bo tęsknię po tobie.") ?>
+        wyrazów
+    </p>
 </body>
+
 </html>

a6454c3ebc1bf1bba004d526190dc59414f14b5e
maciek9972 <85764462+maciek9972@users.noreply.github.com>
2025-02-07 15:06:18 +0100

    Update Wybieranie_liczby.php

--- Html/Zadanie3/Wybieranie_liczby.php
+++ Html/Zadanie3/Wybieranie_liczby.php
@@ -1,18 +1,70 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
+
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        fieldset{
+            border-width: 2px;
+            border-color: blue;
+            margin-left: 600px;
+            margin-right: 600px;
+        }
+        label{
+            margin-right: 15px;
+        }
+        body{
+            padding: 20px;
+        }
+    </style>
 </head>
+
 <body>
     <fieldset>
-    <?php    
-        foreach(range("a","z") as $litera){
+        <legend>Alfabet</legend>
+        <form method="post">
+            <?php
+            foreach (range('a', 'z') as $litera) {
+                $isChecked = (!empty($_POST["litera"]) && in_array($litera, $_POST["litera"])) ? "checked" : "";
+                ?>
+                <input <?= $isChecked ?> type="checkbox" id="litera_<?= $litera ?>" name="litera[]" value="<?= $litera ?>">
+                <label for="litera_<?= $litera ?>"><?= $litera ?></label>
+                <?php
+            }
+            ?>
+            <br>
+            <input type="submit" name="wyslij" value="Wyślij to">
+        </form>
+        <?php
+        if (!empty($_POST["litera"])) {
+            $wybrane = $_POST["litera"];
+            if (count($wybrane) == 1) {
+                ?>
+                <p>
+                    Ta litera została wybrana: <?= $wybrane[0] ?>.
+                </p>
+                <?php
+            } 
+            elseif (count($wybrane) > 1) {
+                ?>
+                <p>
+                    Te litery zostały wybrane: <?= implode(', ', $wybrane) ?>.
+                </p>
+                <?php
+            }
+        } 
+        else {
+            ?>
+            <p>
+                Żadna litera nie została wybrana.
+            </p>
+            <?php
         }
-        
-    ?>
-    <input type="checkbox" name="<?= $litera;?>">
+        ?>
     </fieldset>
 </body>
+
 </html>

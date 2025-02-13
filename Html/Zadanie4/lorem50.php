bcf0ec2b36dd5f81215718a903176453f0202a32
maciek9972 <mdurlej.com@gmail.com>
2025-02-13 15:23:32 +0100

    dsandlasl

---
+++ Html/Zadanie4/lorem50.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+    i{
+        color:red;
+    }
+    </style>
+</head>
+<body>
+    <fieldset><legend>lorem50</legend>
+    <?php if($_SERVER['REQUEST_METHOD']==="POST"){
+    ?><p>Oto wpisany tekst:</p>
+    <p><i><?= $_POST["text"] ?></i></p>
+   <?php }
+    else{
+    ?>
+        <form method="POST">
+        <textarea rows="5" cols="50" name="text" id="text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus reprehenderit dolorem minima quaerat consectetur voluptate officiis minus, rem soluta aut enim, porro ea eius voluptas sunt iusto nobis repudiandae pariatur?</textarea><br>
+        <input type="submit" name="submit" value="Wyślij"></input>
+        </form>
+    </fieldset>
+    <?php } ?>
+   
+</body>
+</html>
---
+++ Html/Zadanie5/najlepszy_kompozytor.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        fieldset{
+            border-color: blue;
+            border-width: 4px;
+        }
+        legend{
+            border-bottom: solid blue 4px;
+            border-top: solid blue 4px;
+        }
+    </style>
+</head>
+<body>
+<body>
+    <fieldset><legend>Najlepszy kompozytor</legend>
+    <?php if($_SERVER['REQUEST_METHOD']==="POST"){
+    ?><p>Według ciebie najlepszym kompozytorem jest:
+    <?= $_POST["komp"] ?></p>
+   <?php }
+    else{
+    ?>
+        <form method="POST">
+            <input required type="radio" id="komp1" name="komp" value="Alexander Bordin"><label for="komp1">Alexander Bordin</label></input><Br>
+            <input required type="radio" id="komp2" name="komp" value="Fryderyk Chopin"><label for="komp2">Fryderyk Chopin</label></input><Br>
+            <input required type="radio" id="komp3" name="komp" value="Edvard Grieg"><label for="komp3">Edvard Grieg</label></input><Br>
+            <input required type="radio" id="komp4" name="komp" value="Stanisław Moniuszko"><label for="komp4">Stanisław Moniuszko</label></input><Br>
+            <input required type="radio" id="komp5" name="komp" value="Giuseppe Verdi"><label for="komp5">Giuseppe Verdi</label></input><Br>
+            <input required type="radio" id="komp6" name="komp" value="Richard Wagner"><label for="komp6">Richard Wagner</label></input><Br>
+        <input type="submit" name="submit" value="Wybierz ulubionego kompozytora"></input><Br>
+        </form>
+    </fieldset>
+    <?php } ?>
+</body>
+</html>
